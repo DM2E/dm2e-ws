b8f3041da13b014d37d0d8a6df6516f9f33c2fd1
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-16 16:47:22 +0100

    AbstractRDFService: Helper functions for URI validation, URI generation, reading a Grafeo from a URI suing content negotiation

--- src/main/java/eu/dm2e/ws/services/data/AbstractRDFService.java
+++ src/main/java/eu/dm2e/ws/services/data/AbstractRDFService.java
@@ -1,19 +1,38 @@
 package eu.dm2e.ws.services.data;
 
-import com.hp.hpl.jena.rdf.model.Model;
-import eu.dm2e.ws.grafeo.Grafeo;
-import eu.dm2e.ws.grafeo.jena.GrafeoImpl;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.*;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Variant;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+import eu.dm2e.ws.grafeo.Grafeo;
+import eu.dm2e.ws.grafeo.jena.GrafeoImpl;
 
 @Produces({ MediaType.TEXT_PLAIN, "application/rdf+xml",
 		"application/x-turtle", "text/turtle", "text/rdf+n3" })
@@ -27,6 +46,10 @@ public abstract class AbstractRDFService {
 	public static final String TTL_A = "application/x-turtle";
 	public static final String TTL_T = "text/turtle";
 	public static final String N3 = "text/rdf+n3";
+	protected static String[] allowedSchemes = { "http", "https", "file", "ftp" };
+	protected static final UrlValidator urlValidator = new UrlValidator(allowedSchemes,
+		UrlValidator.ALLOW_ALL_SCHEMES + UrlValidator.ALLOW_LOCAL_URLS
+	);
 
 	List<Variant> supportedVariants;
 	Map<MediaType, String> mediaType2Language = new HashMap<MediaType, String>();
@@ -46,13 +69,46 @@ public abstract class AbstractRDFService {
 	public Response throwServiceError(Exception e) {
 		return throwServiceError(e.toString(), 400);
 	}
+	
+	protected URI getUriForString(String uriStr) throws URISyntaxException {
+		if (null == uriStr)
+			throw new URISyntaxException(uriStr, "Must provide 'uri' query parameterk.");
+		
+		// this might throw a URISyntaxException as well
+		URI uri = new URI(uriStr);
+		
+		// stricter validation than just throwing an URISyntaxException (Sesame is picky about URLs)
+		if (! validateUri(uriStr)) 
+			throw new URISyntaxException(uriStr, "'uri' parameter is not a valid URI.");
+		
+		return uri;
+		
+	}
 
-	// TODO check if there is a function in uriInfo to do the same
-	protected String getRequestUriString() {
-		String str =  uriInfo.getRequestUri().toString();
-		// remove query string
-		str = str.replaceFirst("\\?.*$", "");
-		return str;
+	protected URI getRequestUriWithoutQuery() {
+		UriBuilder ub =  uriInfo.getRequestUriBuilder();
+		ub.replaceQuery("");
+		return ub.build();
+	}
+	
+	protected Grafeo getGrafeoForUriWithContentNegotiation(String uriStr) throws IOException, URISyntaxException {
+		return getGrafeoForUriWithContentNegotiation(getUriForString(uriStr));
+	}
+	protected Grafeo getGrafeoForUriWithContentNegotiation(URI uri) throws IOException {
+		URL url = null;
+		url = new URL(uri.toString());
+		InputStream in = null;
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Accept", "application/rdf+xml");
+		con.connect();
+		in = con.getInputStream();
+		GrafeoImpl tempG = new GrafeoImpl();
+		tempG.getModel().read(in, null, "RDF/XML");
+		if (tempG.getModel().isEmpty()) {
+			return null;
+		}
+		return tempG;
 	}
 
 	protected AbstractRDFService() {
@@ -92,6 +148,15 @@ public abstract class AbstractRDFService {
 	protected StreamingOutput getResponseEntity(Grafeo grafeo) {
 		return getResponseEntity(((GrafeoImpl) grafeo).getModel());
 
+	}
+
+	protected static boolean validateUri(String uri) {
+		return urlValidator.isValid(uri);
+	}
+	
+	protected String createUniqueStr() {
+		String uniqueStr = new Date().getTime() + "_" + UUID.randomUUID().toString();
+		return uniqueStr;
 	}
 
 	protected class RDFOutput implements StreamingOutput {
