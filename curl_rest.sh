8a6baae7dcdf3da67bd736f7b89d63dcc2f9c373
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-03-17 03:56:44 +0100

    Helper script for testing REST services with curl on the command line

---
+++ curl_rest.sh
@@ -0,0 +1,33 @@
+# Helper aliasses and variables to make testing REST easier
+# Usage:
+# $> source curl_rest.sh
+# $> URL=http:localhost:9998/file
+# $> POST $URL -H $CT_MULTI -H $AC_TURTLE -F file=@payload.xml -F meta=@metadata.ttl
+# instead of
+# $> curl -v -X POST -H "Content-Type: multipart/form-data" -H "Accept: Turtle" -F file=@payload.xml -F meta=@metadata.ttl $URL
+
+alias POST="curl -v -X POST"
+alias GET="curl -v -X GET"
+alias PUT="curl -v -X PUT"
+alias DELETE="curl -v -X DELETE"
+alias PATCH="curl -v -X PATCH"
+alias HEAD="curl -v -X HEAD"
+
+CT_TEXT='Content-Type: text/plain'
+AC_TEXT='Accept: text/plain'
+
+CT_TURTLE='Content-Type: text/turtle'
+AC_TURTLE='Accept: text/turtle'
+
+CT_NTRIPLES='Content-Type: application/rdf-triples'
+AC_NTRIPLES='Accept: application/rdf-triples'
+
+CT_N3='Content-Type: text/rdf+n3'
+AC_N3='Accept: text/rdf+n3'
+
+CT_RDFXML='Content-Type: application/rdf+xml'
+AC_RDFXML='Accept: application/rdf+xml'
+
+CT_MULTI='Content-Type: multipart/form-data'
+AC_MULTI='Accept: multipart/form-data'
+
