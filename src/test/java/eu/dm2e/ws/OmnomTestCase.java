63569c5ee73f7c0531c310610b26e03f1008f42e
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-06-03 00:40:01 +0200

    new test resources, fixed OmnomTestCase

--- src/test/java/eu/dm2e/ws/OmnomTestCase.java
+++ src/test/java/eu/dm2e/ws/OmnomTestCase.java
@@ -1,7 +1,6 @@
 package eu.dm2e.ws;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,13 +20,13 @@ public class OmnomTestCase {
 //	@Before
 //	public void setUp() throws Exception {
 	public OmnomTestCase() {
-		for (OmnomTestResources wsconf : OmnomTestResources.values()) { 
-			URL testConfigURL = this.getClass().getResource(wsconf.getPath());
-			configFile.put(wsconf, new File(testConfigURL.getFile()));
+		for (OmnomTestResources res : OmnomTestResources.values()) { 
+			URL testConfigURL = this.getClass().getResource(res.getPath());
 			try {
-				configString.put(wsconf, IOUtils.toString(testConfigURL.openStream()));
-			} catch (IOException e) {
-				org.junit.Assert.fail(e.toString());
+				configFile.put(res, new File(testConfigURL.getFile()));
+				configString.put(res, IOUtils.toString(testConfigURL.openStream()));
+			} catch (Exception e) {
+				org.junit.Assert.fail(res + e.toString());
 			}
 		}
 	}
--- src/test/java/eu/dm2e/ws/OmnomTestResources.java
+++ src/test/java/eu/dm2e/ws/OmnomTestResources.java
@@ -1,11 +1,16 @@
 package eu.dm2e.ws;
 
 public enum OmnomTestResources {
-	DEMO_JOB("/job/demo_job.ttl")
+	ASCII_NOISE("/random/ascii_noise.txt")
+	, DEMO_JOB("/job/demo_job.ttl")
 	, DEMO_LOG("/job/log_entry.ttl")
 	, DEMO_LOG_WITH_URI("/job/log_entry_uri.ttl")
 	, DEMO_SERVICE_WORKING("/webserviceConfig/demo_config.ttl")
-	, DEMO_SERVICE_BROKEN1("/webserviceConfig/demo_config_no_top_blank.ttl")
+	, DEMO_SERVICE_NO_TOP_BLANK("/webserviceConfig/demo_config_no_top_blank.ttl")
+	, DEMO_SERVICE_ILLEGAL_PARAMETER("/webserviceConfig/demo_config_illegal_sleeptime.ttl")
+	, MINIMAL_FILE("/file/empty_file.ttl")
+	, MINIMAL_FILE_WITH_URI("/file/uri_file.ttl")
+	, ILLEGAL_EMPTY_FILE("/file/illegal_file.ttl")
 	;
 	
 	final String path;
