5fbcb6940bf8cfafabd49d799b93681a3429dc48
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-06-03 00:50:26 +0200

    Client: Helper to create multipart/form-data objects from file/metadata

--- src/main/java/eu/dm2e/ws/services/Client.java
+++ src/main/java/eu/dm2e/ws/services/Client.java
@@ -14,6 +14,7 @@ import com.sun.jersey.multipart.FormDataMultiPart;
 import eu.dm2e.ws.Config;
 import eu.dm2e.ws.DM2E_MediaType;
 import eu.dm2e.ws.api.FilePojo;
+import eu.dm2e.ws.grafeo.Grafeo;
 
 /**
  * This file was created within the DM2E project.
@@ -26,20 +27,19 @@ public class Client {
 
     private com.sun.jersey.api.client.Client jerseyClient = new com.sun.jersey.api.client.Client();
     private Logger log = Logger.getLogger(getClass().getName());
+    
 
     public String publishFile(File file, FilePojo meta) {
-        WebResource fileResource = jerseyClient.resource(Config.getString("dm2e.service.file.base_uri"));
+        WebResource fileResource = getFileWebResource();
         FormDataMultiPart form = new FormDataMultiPart();
-
         // add file part
         MediaType fileType = MediaType.APPLICATION_OCTET_STREAM_TYPE;
         FormDataBodyPart fileFDBP = new FormDataBodyPart("file", file, fileType);
         form.bodyPart(fileFDBP);
 
         // add metadata part
-        FilePojo fileDesc = new FilePojo();
+        FilePojo fileDesc = meta;
         // fileDesc.setGeneratorJob(jobPojo);
-        fileDesc.setMediaType(fileType.toString());
         MediaType n3_type = MediaType.valueOf(DM2E_MediaType.TEXT_RDF_N3);
         FormDataBodyPart metaFDBP = new FormDataBodyPart("meta", fileDesc.getNTriples(), n3_type);
         form.bodyPart(metaFDBP);
@@ -64,16 +64,41 @@ public class Client {
             throw new RuntimeException("We didn't get a location header, ooumph! Status " + resp.getStatus() + ": " + resp.getEntity(String.class));
         }
         // jobPojo.info("File stored at: " + fileLocation);
-        try {
-            fileDesc.setFileRetrievalURI(fileLocation);
-            fileDesc.publish();
-        } catch(Exception e) {
-            // jobPojo.fatal(e);
-            throw new RuntimeException("Error publishing file metadata: " + e, e);
-        }
+//        try {
+//            fileDesc.setFileRetrievalURI(fileLocation);
+//            fileDesc.publish();
+//        } catch(Exception e) {
+//            // jobPojo.fatal(e);
+//            throw new RuntimeException("Error publishing file metadata: " + e, e);
+//        }
         log.info("File stored, URI: " + fileLocation);
         return fileLocation;
     }
+	public FormDataMultiPart createFileFormDataMultiPart(String metaStr, String content) {
+		FormDataMultiPart fdmp = new FormDataMultiPart();
+		if (null == metaStr) {
+			metaStr = "";
+		}
+		FormDataBodyPart fdm_meta = new FormDataBodyPart(
+				"meta", 
+				metaStr,
+				new MediaType("application", "rdf-triples"));
+		fdmp.bodyPart(fdm_meta);
+		if (null != content) {
+			FormDataBodyPart fdm_file = new FormDataBodyPart(
+				"file",
+				content,
+				MediaType.APPLICATION_OCTET_STREAM_TYPE);
+			fdmp.bodyPart(fdm_file);
+		}
+		return fdmp;
+	}
+	public FormDataMultiPart createFileFormDataMultiPart(FilePojo meta, String content) {
+		return createFileFormDataMultiPart(meta.getNTriples(), content);
+	}
+	public FormDataMultiPart createFileFormDataMultiPart(Grafeo meta, String content) {
+		return createFileFormDataMultiPart(meta.getNTriples(), content);
+	}
 //    public HttpResponse postRDF(String postTo, Grafeo g, Map<String,String> headers) {
 //    }
     
@@ -99,5 +124,5 @@ public class Client {
      ********************/
 	public com.sun.jersey.api.client.Client getJerseyClient() { return jerseyClient; }
 	public void setJerseyClient(com.sun.jersey.api.client.Client jerseyClient) { this.jerseyClient = jerseyClient; }
-    
+
 }
