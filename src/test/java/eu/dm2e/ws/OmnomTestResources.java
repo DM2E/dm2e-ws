45d70638afc67a525af6d08097e89d09b52fec1d
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-06-04 14:14:50 +0200

    OmnomTestResources: TEI2EDM_xslt_20130129

--- src/test/java/eu/dm2e/ws/OmnomTestResources.java
+++ src/test/java/eu/dm2e/ws/OmnomTestResources.java
@@ -13,6 +13,7 @@ public enum OmnomTestResources {
 	, ILLEGAL_EMPTY_FILE("/file/illegal_file.ttl")
 	, XML_DTA_GRIMM("/provider-examples/dta/grimm_meistergesang_1811.TEI-P5.xml")
 	, XSLT_KBA_BBAW_TO_EDM("/mappings/xslt/KBA_BBAW_TO_EDM.xsl")
+	, TEI2EDM_20130129("/mappings/xslt-zip/TEI2EDM_xslt_20130129.zip")
 	;
 	
 	final String path;
