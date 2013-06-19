9c512483b0536e72a67b92a454ddd34deb987366
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-06-19 13:34:06 +0200

    WebservicePojo: Implement IWebservice

--- src/main/java/eu/dm2e/ws/api/WebservicePojo.java
+++ src/main/java/eu/dm2e/ws/api/WebservicePojo.java
@@ -1,18 +1,19 @@
 package eu.dm2e.ws.api;
 
-import eu.dm2e.ws.grafeo.Grafeo;
-import eu.dm2e.ws.grafeo.annotations.Namespaces;
-import eu.dm2e.ws.grafeo.annotations.RDFClass;
-import eu.dm2e.ws.grafeo.annotations.RDFId;
-import eu.dm2e.ws.grafeo.annotations.RDFProperty;
-import eu.dm2e.ws.grafeo.jena.GrafeoImpl;
-import org.apache.commons.beanutils.BeanUtils;
-
 import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Logger;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import eu.dm2e.ws.NS;
+import eu.dm2e.ws.grafeo.Grafeo;
+import eu.dm2e.ws.grafeo.annotations.Namespaces;
+import eu.dm2e.ws.grafeo.annotations.RDFClass;
+import eu.dm2e.ws.grafeo.annotations.RDFProperty;
+import eu.dm2e.ws.grafeo.jena.GrafeoImpl;
 
 /**
  * This file was created within the DM2E project.
@@ -25,24 +26,9 @@ import java.util.logging.Logger;
 	"omnom", "http://onto.dm2e.eu/omnom/",
 	"rdfs", "http://www.w3.org/2000/01/rdf-schema#"
 	})
-@RDFClass("omnom:Webservice")
-public class WebservicePojo extends AbstractPersistentPojo<WebservicePojo> {
-
-
-
-//    @RDFId(prefix="http://data.dm2e.eu/data/services/")
-	@RDFId
-    private String id;
+@RDFClass(NS.OMNOM.CLASS_WEBSERVICE)
+public class WebservicePojo extends AbstractPersistentPojo<WebservicePojo> implements IWebservice {
 	
-	@RDFProperty("rdfs:label")
-	private String label;
-
-    @RDFProperty("omnom:inputParam")
-    private Set<ParameterPojo> inputParams = new HashSet<>();
-
-    @RDFProperty("omnom:outputParam")
-    private Set<ParameterPojo> outputParams = new HashSet<>();
-    
     /*********************
      * HELPER FUNCTIONS
      * @return 
@@ -57,20 +43,33 @@ public class WebservicePojo extends AbstractPersistentPojo<WebservicePojo> {
     	return param;
     }
     
-    public ParameterPojo addInputParameter(String paramName) {
+    /* (non-Javadoc)
+	 * @see eu.dm2e.ws.api.IWebservicePojo#addInputParameter(java.lang.String)
+	 */
+    @Override
+	public ParameterPojo addInputParameter(String paramName) {
     	return this.addParameterByName(paramName, false);
     }
-    public ParameterPojo addOutputParameter(String paramName) {
+    /* (non-Javadoc)
+	 * @see eu.dm2e.ws.api.IWebservicePojo#addOutputParameter(java.lang.String)
+	 */
+    @Override
+	public ParameterPojo addOutputParameter(String paramName) {
     	return this.addParameterByName(paramName, true);
     }
     
-    public ParameterPojo getParamByName(String needle) {
+    /* (non-Javadoc)
+	 * @see eu.dm2e.ws.api.IWebservicePojo#getParamByName(java.lang.String)
+	 */
+    @Override
+	public ParameterPojo getParamByName(String needle) {
     	Logger log = Logger.getLogger(getClass().getName());
     	Set<ParameterPojo> allParams = new HashSet<>();
     	allParams.addAll(inputParams);
     	allParams.addAll(outputParams);
     	for (ParameterPojo param : allParams) {
-    		if (param.getId().matches(".*" + needle + "$")) {
+    		log.info(""+param);
+    		if (param.matchesParameterName(needle)) {
     			return param;
     		}
     	}
@@ -88,7 +87,7 @@ public class WebservicePojo extends AbstractPersistentPojo<WebservicePojo> {
 
     public WebservicePojo(URI uri) {
         Grafeo g = new GrafeoImpl(uri.toString());
-        WebservicePojo ws = g.getObjectMapper().getObject(WebservicePojo.class, uri);
+        IWebservice ws = g.getObjectMapper().getObject(WebservicePojo.class, uri);
         try {
             BeanUtils.copyProperties(this, ws);
         } catch (IllegalAccessException | InvocationTargetException e) {
@@ -98,17 +97,16 @@ public class WebservicePojo extends AbstractPersistentPojo<WebservicePojo> {
     /*********************
      * GETTERS/SETTERS
      ********************/
+	
+    @RDFProperty(NS.OMNOM.PROP_INPUT_PARAM)
+    private Set<ParameterPojo> inputParams = new HashSet<>();
+	@Override public Set<ParameterPojo> getInputParams() { return inputParams; }
+	@Override public void setInputParams(Set<ParameterPojo> inputParams) { this.inputParams = inputParams; }
+	
+    @RDFProperty(NS.OMNOM.PROP_OUTPUT_PARAM)
+    private Set<ParameterPojo> outputParams = new HashSet<>();
+	@Override public Set<ParameterPojo> getOutputParams() { return outputParams; }
+	@Override public void setOutputParams(Set<ParameterPojo> outputParams) { this.outputParams = outputParams; }
 
-	public String getId() { return id; }
-	public void setId(String id) { this.id = id; }
-	
-	public String getLabel() { return label; }
-	public void setLabel(String label) { this.label = label; }
-	
-	public Set<ParameterPojo> getInputParams() { return inputParams; }
-	public void setInputParams(Set<ParameterPojo> inputParams) { this.inputParams = inputParams; }
-	
-	public Set<ParameterPojo> getOutputParams() { return outputParams; }
-	public void setOutputParams(Set<ParameterPojo> outputParams) { this.outputParams = outputParams; }
 
 }
