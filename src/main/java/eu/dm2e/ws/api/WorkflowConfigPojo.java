fd2df338aaf5c43ae97a3bbe222e98f271eef6a3
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-06-21 14:24:22 +0200

    WorkflowConfigPojo validates workflows

--- src/main/java/eu/dm2e/ws/api/WorkflowConfigPojo.java
+++ src/main/java/eu/dm2e/ws/api/WorkflowConfigPojo.java
@@ -1,85 +1,126 @@
 package eu.dm2e.ws.api;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
 
+import eu.dm2e.ws.NS;
 import eu.dm2e.ws.grafeo.annotations.Namespaces;
 import eu.dm2e.ws.grafeo.annotations.RDFClass;
-import eu.dm2e.ws.grafeo.annotations.RDFId;
 import eu.dm2e.ws.grafeo.annotations.RDFProperty;
 
 @Namespaces({"omnom", "http://onto.dm2e.eu/omnom/"})
-@RDFClass("omnom:Workflow")
-public class WorkflowConfigPojo implements ConfigPojo {
+@RDFClass(NS.OMNOM.CLASS_WORKFLOW_CONFIG)
+public class WorkflowConfigPojo extends AbstractPersistentPojo<WorkflowConfigPojo> implements IValidatable {
 	
-	public static final String PROP_HAS_POSITION = "omnom:hasPosition";
-	public static final String PROP_PARAMETER_SLOT = "omnom:parameterSlot";
-	Logger log = Logger.getLogger(getClass().getName());
+	public ParameterAssignmentPojo getParameterAssignmentForParam(String paramName) {
+		log.info("Access to param assignment by name: " + paramName);
+        for (ParameterAssignmentPojo ass : this.getParameterAssignments()) {
+        	if (ass.getForParam().matchesParameterName(paramName)) {
+				log.info("GOTCHA : " + ass.getParameterValue()); 
+                return ass;
+			}
+		}
+		return null;
+	}
+	
+	public void addParameterAssignment(String paramName, String paramValue) {
+		ParameterAssignmentPojo ass = new ParameterAssignmentPojo();
+		ParameterPojo param = this.getWorkflow().getParamByName(paramName);
+		ass.setLabel(paramName);
+		ass.setForParam(param);
+		ass.setParameterValue(paramValue);
+		this.getParameterAssignments().add(ass);
+	}
     
     // TODO this is not perfect, there could be several slots for the same position and the same parameter
-    public ParameterSlotPojo getSlotForPositionIndexAndParam(int index, ParameterPojo param) {
-    	WorkflowPositionPojo pos = this.getPositions().get(index);
-    	Logger log = Logger.getLogger(getClass().getName());
-    	if (null != pos) {
-    		for (ParameterSlotPojo thisSlot : parameterSlots) {
-//		    	log.info("" + thisSlot.getForPosition().getWebService());
-//		    	log.info(""+ pos.g);
-    			if (thisSlot.getForPosition() != pos) {
-    				continue;
-    			}
-		    	log.info("" + thisSlot.getForParam().getId());
-    			if (thisSlot.getForParam() != null
-    					&& thisSlot.getForParam().getId().equals(param.getId())
-    					&& thisSlot.getForPosition() == pos) {
-    				return thisSlot;
-    			}
-    		}
-    	}
-    	return null;
-    }
+//    public ParameterConnectorPojo getSlotForPositionIndexAndParam(int index, ParameterPojo param) {
+//    	WorkflowPositionPojo pos = this.getWorkflowPojo().get(index);
+//    	if (null != pos) {
+////    		for (ParameterSlotPojo thisSlot : parameterSlots) {
+//////		    	log.info("" + thisSlot.getForPosition().getWebService());
+//////		    	log.info(""+ pos.g);
+////    			if (thisSlot.getForPosition() != pos) {
+////    				continue;
+////    			}
+////		    	log.info("" + thisSlot.getToParam().getId());
+////    			if (thisSlot.getToParam() != null
+////    					&& thisSlot.getToParam().getId().equals(param.getId())
+////    					&& thisSlot.getForPosition() == pos) {
+////    				return thisSlot;
+////    			}
+////    		}
+//    	}
+//    	return null;
+//    }
 	
+	/**
+	 * Things to make sure:
+	 * a) Every input parameter of the workflow must be covered by an assignment of the workflowconfig
+	 * b) Every required inputParameter of every position is going to be filled somehow
+	 * c) Every output parameter of the workflow is going to be filled somehow
+	 * 
+	 * @see eu.dm2e.ws.api.IValidatable#validate()
+	 */
 	@Override
-	public String toString() {
-		StringBuilder outstr = new StringBuilder();
-		outstr.append("Workflow: [");
-//		outstr.append("{");
-//		for (ParameterPojo param : this.parameters) {
-//			outstr.append(param.getTitle());
-//			outstr.append(": ");
-//			outstr.append(param.getParameterValue());
-//		}
-//		outstr.append("}{");
-		for (WorkflowPositionPojo slot : getPositions()) {
-			outstr.append(slot.getWebServicConfige().getWebservice().getId());
-			outstr.append(" => ");
+	public void validate() {
+		WorkflowPojo wf = this.getWorkflow();
+		log.info("Validating " + this + " with " + wf);
+		
+		// a)
+		for (ParameterPojo param : wf.getInputParams()) {
+			ParameterAssignmentPojo ass = this.getParameterAssignmentForParam(param.getId());
+			if (null == ass) {
+				throw new RuntimeException(param + " is not set by " + this); 
+			}
 		}
-		outstr.append("]");
-		return outstr.toString();
+		
+		// b)
+		// for every position
+		for (WorkflowPositionPojo pos : wf.getPositions()) {
+			WebserviceConfigPojo wsconf = pos.getWebserviceConfig();
+			if (null == wsconf) {
+				throw new AssertionError(pos + " has no WebServiceConfig");
+			}
+			WebservicePojo ws = wsconf.getWebservice();
+			if (null == ws) {
+				throw new AssertionError(wsconf + " of " + pos + " has no webService.");
+			}
+			// for every input parameter of the webservice at this position
+			for (ParameterPojo param : ws.getInputParams()) {
+				if (null != wsconf.getParameterAssignmentForParam(param.getId())) {
+					throw new AssertionError(param + " is covered by the " + wsconf +". This belongs to the Workflow however.");
+				}
+				if (param.getIsRequired()
+						&&
+					null == wf.connectorToPositionAndParam(pos, param)) {
+					throw new RuntimeException(param + " of " + ws + " in " + pos + "is not connected.");
+				}
+			}
+		}
+		
+		// c)
+		outparams:
+		for (ParameterPojo param : wf.getOutputParams()) {
+			if (null != wf.connectorToWorkflowOutputParam(param)) {
+				continue outparams;
+			}
+			throw new RuntimeException("No connector to output parameter " + param + "of workflow " + wf);
+		}
 	}
-
-	@Override
-	public void validateConfig() {
-		log.warning("validateConfig UNIMPLEMENTED HERE");
-	}
-    
+	
     /*********************
      * GETTERS/SETTERS
      ********************/
-
-	// inherited
-    @RDFId()
-    private String id;
 	
-    @RDFProperty(PROP_HAS_POSITION) private List<WorkflowPositionPojo> positions = new ArrayList<>();
-	public List<WorkflowPositionPojo> getPositions() { return positions; }
-	public void setPositions(List<WorkflowPositionPojo> positions) { this.positions = positions; }
-    
-    @RDFProperty(PROP_PARAMETER_SLOT) private Set<ParameterSlotPojo> parameterSlots = new HashSet<>();
-	public Set<ParameterSlotPojo> getParameterSlots() { return parameterSlots; }
-	public void setParameterSlots(Set<ParameterSlotPojo> parameterSlots) { this.parameterSlots = parameterSlots; }
-
+    @RDFProperty(value = NS.OMNOM.PROP_WORKFLOW )
+    private WorkflowPojo workflow;
+	public WorkflowPojo getWorkflow() { return workflow; }
+	public void setWorkflow(WorkflowPojo wf) { this.workflow = wf; }
 	
+	@RDFProperty(NS.OMNOM.PROP_ASSIGNMENT)
+	private Set<ParameterAssignmentPojo> parameterAssignments = new HashSet<>();
+	public Set<ParameterAssignmentPojo> getParameterAssignments() { return parameterAssignments; }
+	public void setParameterAssignments(Set<ParameterAssignmentPojo> parameterAssignments) { this.parameterAssignments = parameterAssignments; }
+
+
 }
