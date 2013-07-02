e4e435ec4249bd9d33f88c400ed4731a33e0af01
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-07-02 02:55:51 +0200

    LogEntryPojoTest

--- src/test/java/eu/dm2e/ws/api/LogEntryPojoTest.java
+++ src/test/java/eu/dm2e/ws/api/LogEntryPojoTest.java
@@ -3,12 +3,15 @@ package eu.dm2e.ws.api;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.eclipse.jetty.util.log.Log;
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.gson.JsonObject;
+
 import eu.dm2e.ws.OmnomUnitTest;
+import eu.dm2e.ws.api.json.SerializablePojoJsonSerializer;
+import eu.dm2e.ws.grafeo.annotations.RDFClass;
 
 public class LogEntryPojoTest extends OmnomUnitTest{
 	private String id = "http://log.foo/bar/1234";
@@ -43,6 +46,50 @@ public class LogEntryPojoTest extends OmnomUnitTest{
 		logentry.setLevel(level);
 		logentry.setTimestamp(timestamp);
 		assertEquals(asCanonicalNTriples, logentry.getCanonicalNTriples());
+	}
+	
+	@Test
+	public void testSerializeJson() {
+		DateTime dt = DateTime.now();
+		LogEntryPojo logE = new LogEntryPojo();
+		logE.setTimestamp(dt);
+		logE.setMessage("foo");
+			
+		{
+			JsonObject expected = new JsonObject();
+			expected.addProperty("message", "foo");
+			expected.addProperty("timestamp", dt.toString());
+			expected.addProperty(SerializablePojoJsonSerializer.FIELD_RDF_TYPE, logE.getRDFClassUri());
+			assertStringsEqual(testGson.toJson(expected), jsonSerializer.serializeToJSON(logE));
+		}
+		{
+			String id = "http://foo";
+			logE.setId(id);
+			JsonObject expected = new JsonObject();
+			expected.addProperty("message", "foo");
+			expected.addProperty("timestamp", dt.toString());
+			expected.addProperty(SerializablePojoJsonSerializer.FIELD_ID, id);
+			expected.addProperty(SerializablePojoJsonSerializer.FIELD_RDF_TYPE, logE.getRDFClassUri());
+			assertStringsEqual(testGson.toJson(expected), jsonSerializer.serializeToJSON(logE));
+		}
+	}
+	
+	
+
+	@Test 
+	public void testDeserializeJson() {
+		DateTime dt = DateTime.now();
+		JsonObject asJson = new JsonObject();
+		asJson.addProperty("message", "foo");
+		asJson.addProperty("timestamp", dt.toString());
+		asJson.addProperty(SerializablePojoJsonSerializer.FIELD_RDF_TYPE, LogEntryPojo.class.getAnnotation(RDFClass.class).value());
+		LogEntryPojo logE = jsonSerializer.deserializeFromJSON(LogEntryPojo.class, asJson.toString());
+		
+		LogEntryPojo expected = new LogEntryPojo();
+		expected.setMessage("foo");
+		expected.setTimestamp(dt);
+		assertEquals(expected, logE);
+		assertEquals(testGson.toJson(asJson), jsonSerializer.serializeToJSON(logE));
 	}
 	
 //	@Test
