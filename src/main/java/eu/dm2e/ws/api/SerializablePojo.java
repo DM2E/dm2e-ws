6b6f99be4352e30c7a402d3e4e30771cfbad1a75
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-07-02 02:18:36 +0200

    SerializablePojo: hashCode + equals

--- src/main/java/eu/dm2e/ws/api/SerializablePojo.java
+++ src/main/java/eu/dm2e/ws/api/SerializablePojo.java
@@ -129,4 +129,34 @@ public abstract class SerializablePojo<T> {
 	public void setLabel(String label) { this.label = label; }
 	public boolean hasLabel() { return this.label != null; }
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (!(obj instanceof SerializablePojo)) return false;
+		SerializablePojo other = (SerializablePojo) obj;
+		if (id == null) {
+			if (other.id != null) return false;
+		} else if (!id.equals(other.id)) return false;
+		if (label == null) {
+			if (other.label != null) return false;
+		} else if (!label.equals(other.label)) return false;
+		return true;
+	}
+
 }
