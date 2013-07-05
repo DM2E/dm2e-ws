0d26d0c7978e26c0839c353fb181b4c74c2ca727
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-07-05 02:04:40 +0200

    Colorful log output on the console

---
+++ src/main/java/eu/dm2e/logback/HighlightHighContrastConverter.java
@@ -0,0 +1,23 @@
+package eu.dm2e.logback;
+
+import static ch.qos.logback.core.pattern.color.ANSIConstants.*;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+
+
+public class HighlightHighContrastConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
+
+	@Override
+	protected String getForegroundColorCode(ILoggingEvent event) {
+		Level level = event.getLevel();
+		switch (level.toInt()) {
+			case Level.ERROR_INT: return BOLD+RED_FG;
+			case Level.WARN_INT: return BOLD+RED_FG;
+			case Level.INFO_INT: return BOLD+GREEN_FG;
+			case Level.DEBUG_INT: return BOLD+YELLOW_FG;
+			default: return DEFAULT_FG;
+		}
+	}
+
+}
