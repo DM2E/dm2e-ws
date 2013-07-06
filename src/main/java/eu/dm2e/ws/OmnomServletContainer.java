76b66f240a0b255ee9924b36caa1623277d0b2c9
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-07-06 17:18:44 +0200

    ServletContainer extending Jersey's ServletContainer

---
+++ src/main/java/eu/dm2e/ws/OmnomServletContainer.java
@@ -0,0 +1,35 @@
+package eu.dm2e.ws;
+
+import javax.servlet.ServletException;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+import eu.dm2e.ws.wsmanager.ManageService;
+
+public class OmnomServletContainer extends ServletContainer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init() throws ServletException {
+//		ManageService.startFuseki();
+		super.init();
+	}
+	
+	@Override
+	public void reload() {
+//		ManageService.startFuseki();
+		super.reload();
+//		ManageService.stopFusekiServer();
+	}
+	
+	@Override
+	public void destroy() {
+//		ManageService.stopFusekiServer();
+		super.destroy();
+	}
+
+}
