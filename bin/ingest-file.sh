246652a3935086a64378001ec6dc8ca52106b78b
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-07-19 02:54:50 +0200

    logging resources, script to ingest files, curl_rest.sh shell config

--- bin/ingest-file.sh
+++ bin/ingest-file.sh
@@ -4,9 +4,6 @@ source curl_rest.sh
 
 file=$1
 
-SRV_MAIN="http://localhost:8080/dm2e-ws/api"
-
-file_location_line=$(POST $SRV_MAIN/file -F file=@$file 2>&1| grep 'Location: ' |sed 's///')
+file_location_line=$(POST $SRV/file -F file=@$file 2>&1| grep 'Location: ' |sed 's///')
 file_uri=$(echo $file_location_line | grep -o 'http.*')
-
 echo $file_uri
