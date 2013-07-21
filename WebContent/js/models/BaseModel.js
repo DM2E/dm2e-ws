407ab7ddf58c9077ebb4327ace94e32ced624acd
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-07-21 17:45:57 +0200

    absoluteURL of models in BaseModel/RelationalModel

--- WebContent/js/models/BaseModel.js
+++ WebContent/js/models/BaseModel.js
@@ -14,7 +14,20 @@ define([
 	var theDefaults = {};
 
 	return Backbone.Model.extend({
-		
+
+        absoluteURL : function() {
+            var baseURL = (typeof this.url === 'function')
+                ? this.url()
+                : this.url;
+            if (/^(https?|ftp):\/\/.*/.test(baseURL)) {
+                return baseURL;
+            }
+            return window.location.protocol
+                + "//"
+                + window.location.host
+                + (/^\/.*/.test(baseURL) ? "" : "/")
+                + baseURL;
+        },
 		getQN : function(qname) {
 			var url = NS.getQN(qname);
 			if (! url) throw "Unknown QName " + qname;
