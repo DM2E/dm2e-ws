3982f27722aa8bc73e708fb83119a5da783a5e5c
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-10-07 10:20:45 +0200

    UriUtils: functions for url encoding/ input sanitizing, DM2E/dm2e-gu#37

--- src/main/java/eu/dm2e/utils/UriUtils.java
+++ src/main/java/eu/dm2e/utils/UriUtils.java
@@ -1,10 +1,33 @@
 package eu.dm2e.utils;
 
+import org.eclipse.jetty.util.UrlEncoded;
+
 public class UriUtils {
 	
 	public static String lastUriSegment(String uri) {
 		String[] uriRev = uri.split("/");
 		return uriRev[uriRev.length - 1];
+	}
+	
+	/**
+	 * Cleans up input for use in web services.
+	 * 
+	 * TODO / BUG: Since we don't discern URI parameters from String parameters cleanly
+	 * ATM, we cannot just URL-encode the String (because that would break URLs).
+	 * Instead we cover the most important cases here.
+	 * 
+	 * TODO: Investigate whether and how risky this is.
+	 * 
+	 * @param uri
+	 * @return
+	 */
+	public static String sanitizeInput(String uri) {
+		uri = uri.replaceAll("\\s", "_");
+		return uri;
+	}
+	
+	public static String uriEncode(String uri) {
+		return UrlEncoded.encodeString(uri);
 	}
 
 }
