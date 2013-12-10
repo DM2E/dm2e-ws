795d235c5a4dfa8308b62c92c8eac5bfabd47d3c
Konstantin Baierer <konstantin.baierer@gmail.com>
2013-12-10 15:29:55 +0100

    Fixed ClientITCase

--- src/test/java/eu/dm2e/ws/tests/integration/services/ClientITCase.java
+++ src/test/java/eu/dm2e/ws/tests/integration/services/ClientITCase.java
@@ -7,8 +7,10 @@ import eu.dm2e.ws.Config;
 import eu.dm2e.ws.ConfigProp;
 import eu.dm2e.ws.DM2E_MediaType;
 import eu.dm2e.ws.api.FilePojo;
+import eu.dm2e.ws.api.UserPojo;
 import eu.dm2e.ws.services.Client;
 import eu.dm2e.ws.tests.OmnomTestCase;
+
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -17,16 +19,20 @@ import org.junit.Test;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
 import org.joda.time.DateTime;
+
 import eu.dm2e.logback.LogbackMarkers;
 
 public class ClientITCase extends OmnomTestCase {
@@ -41,6 +47,10 @@ public class ClientITCase extends OmnomTestCase {
 	private boolean isCorrectlyPosted(String uri) {
 		Invocation invoc = client.target(uri).request().build("GET");
 //		log.debug("" + invoc.);
+		Response resp = invoc.invoke();
+		if (resp.getStatus() == Response.Status.TEMPORARY_REDIRECT.getStatusCode()) {
+			invoc = client.target(resp.getLocation()).request().build("GET");
+		}
 		String respStr = invoc.invoke(String.class);
 		assertThat(respStr, is(FILE_CONTENTS));
 		return respStr.equals(FILE_CONTENTS);
@@ -57,10 +67,12 @@ public class ClientITCase extends OmnomTestCase {
 		}
 		assertTrue(fp.getGrafeo().size() > 0);
 		fp.setModified(DUMMY_TIME);
-		fp.setFileRetrievalURI("http://dummy");
+		fp.setFileRetrievalURI((URI)null);
+		fp.setInternalFileLocation(null);
+		fp.setCreated(null);
 		log.debug(LogbackMarkers.DATA_DUMP, "METADATA WAS: " + fp.getGrafeo().getTerseTurtle());
 		log.debug(LogbackMarkers.DATA_DUMP, "METADATA EXPECTED: " + FILE_POJO.getGrafeo().getTerseTurtle());
-		GrafeoAssert.graphContainsGraphStructurally(fp.getGrafeo(), FILE_POJO.getGrafeo());
+		GrafeoAssert.graphContainsGraphStructurally(FILE_POJO.getGrafeo(), fp.getGrafeo());
 
 		return true;
 //		if (null == fp.getOriginalName()
@@ -77,6 +89,11 @@ public class ClientITCase extends OmnomTestCase {
         client = new Client();
 		FILE_POJO.setOriginalName(ORIGINAL_NAME);
 		FILE_POJO.setModified(DUMMY_TIME);
+		FILE_POJO.setFileOwner(new UserPojo());
+//		FILE_POJO.setFileRetrievalURI("http://dummy");
+		FILE_POJO.setFileType("someType");
+		FILE_POJO.setExtent(13L);
+		FILE_POJO.setFormat("application/octet-stream");
 		TEMP_FILE = File.createTempFile("omnom_test", "txt");
 		IOUtils.copy(IOUtils.toInputStream(FILE_CONTENTS), new FileOutputStream(TEMP_FILE));
 	}
