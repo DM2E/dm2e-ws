ae0cc32c294e9ccca251ec715fd6ec2f5faf8fe2
Konstantin Baierer <konstantin.baierer@gmail.com>
2014-01-31 15:04:45 +0100

    More Documentation on our ontology

--- src/main/java/eu/dm2e/ws/NS.java
+++ src/main/java/eu/dm2e/ws/NS.java
@@ -29,7 +29,8 @@ public final class NS {
 	@Retention(RetentionPolicy.RUNTIME)
 	public @interface OWLAnnotation {
 		String description();
-		String owlType() default NS.OWL.CLASS; // can be NS.OWL.DATATYPE_PROPERTY, NS.OWL.OBJECT_PROPERTY or NS.OWL.CLASS
+		String owlType() default NS.OWL.CLASS; // can be NS.OWL.DATATYPE_PROPERTY, NS.OWL.OBJECT_PROPERTY, NS.OWL.INSTANCE or NS.OWL.CLASS
+		String rdfType() default NS.OWL.THING;
 		String domain() default "";
 		String range() default "";
 		String label() default "";
@@ -58,19 +59,19 @@ public final class NS {
 		@OWLAnnotation(description="A class representing a web service parameter.")
         public static final String CLASS_PARAMETER            = BASE + "Parameter";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="An assignement of a concrete value to a Parameter")
         public static final String CLASS_PARAMETER_ASSIGNMENT = BASE + "ParameterAssignment";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A connector between input/output of Parameters of two Webservices")
         public static final String CLASS_PARAMETER_CONNECTOR  = BASE + "ParameterConnector";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A webservice")
         public static final String CLASS_WEBSERVICE           = BASE + "Webservice";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A configuration of a Webservice, i.e. a set of ParameterAssignments")
         public static final String CLASS_WEBSERVICE_CONFIG    = BASE + "WebserviceConfig";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A workflow")
         public static final String CLASS_WORKFLOW             = BASE + "Workflow";
         
 		@OWLAnnotation( description = "A job representing the execution of a Workflow with a specific configuration.",
@@ -95,38 +96,73 @@ public final class NS {
 				range = NS.XSD.ANY_URI)
 		public static final String PROP_FILE_EDIT_URI       = BASE + "fileEditURI";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The internal file location of a file on the server",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.STRING)
 		public static final String PROP_FILE_LOCATION       = BASE + "internalFileLocation";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The resolvable URI whence the file contents can be retrieved.",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.ANY_URI)
 		public static final String PROP_FILE_RETRIEVAL_URI  = BASE + "fileRetrievalURI";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The status of a file. Can be one of AVAILABLE (the file can be retrieved), "
+				+ "WAITING (the file is not yet ready) or "
+				+ "DELETED (the file was deleted)",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.STRING)
 		public static final String PROP_FILE_STATUS         = BASE + "fileStatus";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The file type of a File in terms of the omnom_types ontology.",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.OMNOM_TYPES.CLASS_TYPE
+				)
 		public static final String PROP_FILE_TYPE           = BASE + "fileType";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A completeted Webservice Job of this Workflow Job",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_JOB,
+				range=NS.OMNOM.CLASS_JOB)
 		public static final String PROP_FINISHED_JOB        = BASE + "finishedJobs";
 //		public static final String PROP_FINISHED_POSITION   = BASE + "finishedPositions";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The Parameter this ParameterAssignment assigns a value for",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_ASSIGNMENT,
+				range=NS.OMNOM.CLASS_PARAMETER)
 		public static final String PROP_FOR_PARAM           = BASE + "forParam";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="To Be Deleted",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				deprecated=true)
 		public static final String PROP_FOR_SLOT            = BASE + "forSlot";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The originating Parameter for this ParameterConnector.",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_CONNECTOR,
+				range=NS.OMNOM.CLASS_PARAMETER)
 		public static final String PROP_FROM_PARAM          = BASE + "fromParam";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The originating WorkflowPosition of this ParameterConnector.",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_CONNECTOR,
+				range=NS.OMNOM.CLASS_WORKFLOW_POSITION)
 		public static final String PROP_FROM_POSITION       = BASE + "fromPosition";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The originating Workflow of this ParameterConnector.",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_CONNECTOR,
+				range=NS.OMNOM.CLASS_WORKFLOW)
 		public static final String PROP_FROM_WORKFLOW       = BASE + "fromWorkflow";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="An input Parameter for this Webservice",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_WEBSERVICE,
+				range=NS.OMNOM.CLASS_PARAMETER)
 		public static final String PROP_INPUT_PARAM         = BASE + "inputParam";
 
 		@OWLAnnotation(description="TODO")
@@ -135,73 +171,135 @@ public final class NS {
 		@OWLAnnotation(description="TODO")
         public static final String PROP_HAS_ITERATIONS      = BASE + "hasIterations";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Whether an assignment for this Parameter is required to create a valid WebserviceConfig",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_WEBSERVICE,
+				range=NS.XSD.BOOLEAN)
         public static final String PROP_IS_REQUIRED         = BASE + "isRequired";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="(MINT) XPath to get the item label.",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.STRING)
         public static final String PROP_ITEM_LABEL_XPATH    = BASE + "itemLabelXPath";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="(MINT) XPath to get the item root.",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.STRING)
 		public static final String PROP_ITEM_ROOT_XPATH     = BASE + "itemRootXPath";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The temporary ID jobs get assigned while they are part of a workflow job",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_JOB,
+				range=NS.XSD.STRING)
         public static final String PROP_JOB_TEMPID          = BASE + "temporaryID";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Job status. Can be one of NOT_STARTED, STARTED, ITERATING, WAITING, FINISHED or FAILED",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_JOB,
+				range=NS.XSD.STRING)
         public static final String PROP_JOB_STATUS          = BASE + "status";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The set of started Jobs",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_JOB,
+				range=NS.OMNOM.CLASS_JOB)
         public static final String PROP_JOB_STARTED         = BASE + "startedJobs";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The parent Job of this Job.",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_JOB,
+				range=NS.OMNOM.CLASS_JOB)
         public static final String PROP_JOB_PARENT          = BASE + "parentJob";
 
 		@OWLAnnotation(description="TODO")
         public static final String PROP_JOB_LATEST_RESULT   = BASE + "latestResult";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Referring to the representation of a single logging event",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_JOB,
+				range=NS.OMNOM.CLASS_LOG_ENTRY)
         public static final String PROP_LOG_ENTRY           = BASE + "logEntries";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Log level. One of TRACE, DEBUG, INFO, WARN, ERROR or FATAL.",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_LOG_ENTRY,
+				range=NS.XSD.STRING)
 		public static final String PROP_LOG_LEVEL           = BASE + "hasLogLevel";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A string to be logged",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_LOG_ENTRY,
+				range=NS.XSD.STRING)
 		public static final String PROP_LOG_MESSAGE         = BASE + "hasLogMessage";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Refers to the hexadecimal representation of a file",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.STRING
+				)
 		public static final String PROP_MD5                 = BASE + "md5";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The original name of an uploaded file",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.XSD.STRING)
 		public static final String PROP_ORIGINAL_NAME       = BASE + "originalName";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Referring to an output parameter of a Webservice",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_WEBSERVICE,
+				range=NS.OMNOM.CLASS_PARAMETER)
 		public static final String PROP_OUTPUT_PARAM        = BASE + "outputParam";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Referring to a ParameterConnector of this Workflow",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_WORKFLOW,
+				range=NS.OMNOM.CLASS_PARAMETER_CONNECTOR)
 		public static final String PROP_PARAMETER_CONNECTOR = BASE + "parameterConnector";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The type of parameter value this parameter expects.",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER,
+				range=NS.XSD.ANY_URI)
 		public static final String PROP_PARAMETER_TYPE      = BASE + "parameterType";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The parameter value of a ParameterAssignment",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_ASSIGNMENT)
         public static final String PROP_PARAMETER_VALUE     = BASE + "parameterValue";
 
 		@OWLAnnotation(description="TODO")
         public static final String PROP_PARAMETER_SERIAL     = BASE + "parameterSerial";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Referring to a WorkflowPosition within a workflow",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_WORKFLOW,
+				range=NS.OMNOM.CLASS_WORKFLOW_POSITION)
         public static final String PROP_WORKFLOW_POSITION   = BASE + "workflowPosition";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The target Parameter of a ParameterConnector",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_CONNECTOR,
+				range=NS.OMNOM.CLASS_PARAMETER)
 		public static final String PROP_TO_PARAM            = BASE + "toParam";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The target WorkflowPosition of a ParameterConnector",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_CONNECTOR,
+				range=NS.OMNOM.CLASS_WORKFLOW_POSITION)
 		public static final String PROP_TO_POSITION         = BASE + "toPosition";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The target Workflow of a ParameterConnector",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_PARAMETER_CONNECTOR,
+				range=NS.OMNOM.CLASS_WORKFLOW)
 		public static final String PROP_TO_WORKFLOW         = BASE + "toWorkflow";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Referring to a Webservice",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				range=NS.OMNOM.CLASS_WEBSERVICE)
         public static final String PROP_WEBSERVICE          = BASE + "webservice";
 
 		@OWLAnnotation(description="TODO")
@@ -210,30 +308,44 @@ public final class NS {
 		@OWLAnnotation(description="TODO")
         public static final String PROP_EXEC_WEBSERVICE          = BASE + "isExecutableAt";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Referring to the WebserviceConfig of a Webservice",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				range=NS.OMNOM.CLASS_WEBSERVICE_CONFIG)
         public static final String PROP_WEBSERVICE_CONFIG   = BASE + "webserviceConfig";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="To Be Deleted",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				deprecated=true)
 		public static final String PROP_WORKFLOW            = BASE + "workflow";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="A registered FileService for an Omnom User",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.FOAF.CLASS_PERSON,
+				range=NS.OMNOM.CLASS_WEBSERVICE)
 		public static final String PROP_FILE_SERVICE = BASE + "fileservice";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The user who uploaded a file and hence owns it as far as the system is concerned",
+				owlType=NS.OWL.OBJECT_PROPERTY,
+				domain=NS.OMNOM.CLASS_FILE,
+				range=NS.FOAF.CLASS_PERSON)
 		public static final String PROP_FILE_OWNER = BASE + "fileOwner";
 
 		@OWLAnnotation(description="TODO")
 		public static final String PROP_RUNNING_JOB = BASE + "runningJob";
 
-//		public static final String PROP_FAILED_JOB = BASE + "failedJob";
-
 		@OWLAnnotation(description="TODO")
 		public static final String PROP_EXECUTES_POSITION = BASE + "executesPosition";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="The preferred theme of a user. Valid values are 'light' and 'dark'.",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.FOAF.CLASS_PERSON,
+				range=NS.XSD.STRING)
 		public static final String PROP_PREFERRED_THEME     = BASE + "preferredTheme";
 
-		@OWLAnnotation(description="TODO")
+		@OWLAnnotation(description="Whether or not a user has his 'My Stuff' filter set",
+				owlType=NS.OWL.DATATYPE_PROPERTY,
+				domain=NS.FOAF.CLASS_PERSON,
+				range=NS.XSD.BOOLEAN)
 		public static final String PROP_GLOBAL_USER_FILTER     = BASE + "globalUserFilter";
 
 		@OWLAnnotation(description="TODO")
@@ -246,12 +358,35 @@ public final class NS {
 	public static final class OMNOM_TYPES {
 
 		public static final String BASE    = "http://onto.dm2e.eu/omnom-types/";
+		@OWLAnnotation(description="Base class of all file types")
+		public static final String CLASS_TYPE = BASE + "Type";
+		@OWLAnnotation(description="Gzipped Tarball",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String TGZ     = BASE + "TGZ";
+		@OWLAnnotation(description="Gzipped Tarball containing exactly one XML file.",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String TGZ_XML = BASE + "TGZ-XML";
+		@OWLAnnotation(description="Single XML file.",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String XML     = BASE + "XML";
+		@OWLAnnotation(description="Single XSLT script.",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String XSLT    = BASE + "XSLT";
+		@OWLAnnotation(description="Zipped folder of XML files",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String ZIP_XML = BASE + "ZIP-XML";
+		@OWLAnnotation(description="Zipped folder of a set of XSLT scripts with exactly one containing the root template.",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String ZIP_XSLT= BASE + "ZIP-XSLT";
+		@OWLAnnotation(description="Fallback class for unknown file types.",
+				owlType=NS.OWL.CLASS,
+				rdfType=NS.OMNOM_TYPES.CLASS_TYPE)
 		public static final String UNKNOWN = BASE + "UNKNOWN";
 	}
 
@@ -352,6 +487,7 @@ public final class NS {
 		public static final String BASE = "http://www.w3.org/2001/XMLSchema#";
 		public static final String INT  = BASE + "int";
 		public static final String STRING = BASE + "string";
+		public static final String BOOLEAN = BASE + "boolean";
 		public static final String ANY_URI = BASE + "anyURI";
 	}
 
@@ -423,9 +559,12 @@ public final class NS {
 	}
 	public static final class OWL {
 		public static final String BASE = "http://www.w3.org/2002/07/owl#";
+		public static final String THING = BASE + "Thing";
 		public static final String CLASS = BASE + "Class";
+		public static final String INDIVIDUAL = BASE + "Individual";
 		public static final String DATATYPE_PROPERTY = BASE + "DatatypeProperty";
 		public static final String OBJECT_PROPERTY = BASE + "ObjectProperty";
+		public static final String DEPRECATED = BASE + "deprecated";
 	}
 
 
